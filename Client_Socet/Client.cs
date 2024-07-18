dde73e79076d4c68721b802a9344c1ce4f9439fb
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-07-18 14:05:04 +0300

    initiall commit

--- Client_Socet/Client.cs
+++ Client_Socet/Client.cs
@@ -1,10 +1,49 @@
-﻿namespace Client_Socet
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // Client settings
+        IPAddress ipAddress = IPAddress.Parse("192.168.0.103");
+        int port = 11000;
+        IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+        // Create a socket
+        Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        try
         {
-            Console.WriteLine("Hello, World!");
+            // Connect to the server
+            sender.Connect(remoteEP);
+
+            Console.WriteLine($"Connected to {sender.RemoteEndPoint.ToString()}");
+
+            // Send data to the server
+            byte[] msg = Encoding.UTF8.GetBytes("Hello, server!");
+            int bytesSent = sender.Send(msg);
+
+            // Receive the response from the server
+            byte[] bytes = new byte[1024];
+            int bytesRec = sender.Receive(bytes);
+            string data = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+            // Display the received message
+            Console.WriteLine($"At {DateTime.Now.ToShortTimeString()} received from {((IPEndPoint)sender.RemoteEndPoint).Address}: {data}");
+
+            // Close the connection
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
         }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
     }
 }
