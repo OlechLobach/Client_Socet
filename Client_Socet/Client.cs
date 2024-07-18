291f830bc01c335d26933401917c1fae3d958480
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-07-18 14:32:00 +0300

    initiall commit

--- Client_Socet/Client.cs
+++ Client_Socet/Client.cs
@@ -1,10 +1,45 @@
-﻿namespace Client_Socet
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Client
 {
-    internal class Program
+    static async Task Main(string[] args)
     {
-        static void Main(string[] args)
+        IPAddress ipAddress = IPAddress.Parse("192.168.0.103");
+        int port = 11000;
+        IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+        Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        try
         {
-            Console.WriteLine("Hello, World!");
+            await sender.ConnectAsync(remoteEP);
+            Console.WriteLine($"Connected to {sender.RemoteEndPoint.ToString()}");
+
+            Console.WriteLine("Enter 'time' to request the current time or 'date' to request the current date:");
+            string request = Console.ReadLine();
+
+            byte[] msg = Encoding.UTF8.GetBytes(request);
+            await sender.SendAsync(new ArraySegment<byte>(msg), SocketFlags.None);
+
+            byte[] bytes = new byte[1024];
+            int bytesRec = await sender.ReceiveAsync(new ArraySegment<byte>(bytes), SocketFlags.None);
+            string data = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+            Console.WriteLine($"At {DateTime.Now.ToShortTimeString()} received: {data}");
+
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
         }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
     }
 }
